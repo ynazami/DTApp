6f5dfd1353e27e4a32ab1be69b3468af02baa54b
ynazami <32819587+ynazami@users.noreply.github.com>
2018-12-02 12:03:22 -0700

    initial commit

---
+++ DTApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+           _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var data = await _context.Values.ToListAsync();
+            return Ok(data);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var data = await _context.Values.Where(p => p.Id == id).SingleOrDefaultAsync();
+            return Ok(data);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DTApp.API/Data/Data.cs
@@ -0,0 +1,12 @@
+using DTApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTApp.API.Data
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+            
+        public DbSet<Value> Values { get; set; }
+    }
+}
